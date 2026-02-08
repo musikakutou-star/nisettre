6761c7da00b7c28625bdbdae9bcfcec8046feab0
musikakutou-star <musikakutou@gmail.com>
2026-02-08 23:22:51 +0900

    git練習

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,12 +16,12 @@
 <body class="min-h-screen flex flex-col bg-base-200 font-sans">
     <nav class="navbar bg-base-100">
         <div class="navbar-start">
-            <a href="/" class="btn btn-ghost text-xl">🐦🐦🐦🐦🐦🐦🐦🐦🦀🦐🍅🐞Chirper</a>
+            <a href="/" class="btn btn-ghost text-xl">🐦🐦🐦🐦🐦🐦🐦🐦🦀🦐🍅🐞🍶Chirper</a>
         </div>
         <div class="navbar-end gap-2">
             <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
             <a href="#" class="btn btn-primary btn-sm">Sign Up</a>
-        </div>
+        </div>🍶
     </nav>
 
     <main class="flex-1 container mx-auto px-4 py-8">
