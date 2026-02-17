f129bdc290b219eb29aa759906dfc037c664a979
musikakutou-star <musikakutou@gmail.com>
2026-02-17 23:05:21 +0900

    add #14 chirpsテーブルの内容を画面に表示させるところまで実装

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chirp;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(50)  // Limit to 50 most recent chirps
+            ->get();
+
+        return view('home', ['chirps' => $chirps]);
     }
 
     /**
