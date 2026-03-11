07576e177ec1bdee69e7cb637d22f38ab415b1ad
musikakutou-star <musikakutou@gmail.com>
2026-03-12 01:00:15 +0900

    add #31 ホーム画面実装中

--- database/migrations/2026_03_10_225649_create_recipes_table.php
+++ database/migrations/2026_03_10_225649_create_recipes_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('cooking');
+            $table->text('memo');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RecipeSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +22,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call(RecipeSeeder::class);
     }
 }
